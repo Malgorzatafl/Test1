54706fcf0685c8a677ea53c6b7e1b5effb851a1a
Malgorzatafl <tun-sij@hotmail.com>
2019-11-24 09:19:40 +0100

    Initial commit

---
+++ src/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static Double cenaBrutto{Double cena){
+    return cena*1.23;
+
+
+    }
+}
+public static void main{String
+    }
---
+++ src/Mane.java
@@ -0,0 +1,12 @@
+import java.util.AbstractMap;
+
+public class Mane {
+   public static String zwrocImie
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
